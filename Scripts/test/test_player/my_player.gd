0445cad97413a35d0699de916724f68c40e8c528
raviwhitehat <ekravi26@gmail.com>
2026-03-18 20:46:59 +0530

    Enhance game mechanics by adding ray casting functionality and updating environment settings

--- Scripts/test/test_player/my_player.gd
+++ Scripts/test/test_player/my_player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 const SPEED = 16
+@onready var ray_cast: RayCast2D = $RayCast
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var dir = Input.get_vector("move_left","move_right","move_up","move_down")
 	
 	if dir != Vector2.ZERO:
@@ -10,3 +11,21 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = Vector2.ZERO
 	move_and_slide()
+
+func _process(_delta: float) -> void:
+	ray_cast.target_position = get_local_mouse_position()
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		snap_photo()
+
+func snap_photo():
+	ray_cast.force_raycast_update()
+	
+	if ray_cast.is_colliding():
+		var hit_obj = ray_cast.get_collider()
+		
+		if hit_obj.is_in_group("GlitchObjects"):
+			print("Glitch Object Captured")
+			hit_obj.queue_free()
+		else:
+			print("Just a Normal Photo")
