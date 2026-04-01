8dccffec255425c541a33a7381ad4ecbc3577644
raviwhitehat <ekravi26@gmail.com>
2026-04-01 21:25:14 +0530

    Refactor game scene and player script to integrate HUD elements and manage battery functionality

--- Scripts/test/test_player/my_player.gd
+++ Scripts/test/test_player/my_player.gd
@@ -5,8 +5,22 @@ const SPEED = 16
 @onready var lens_mask: Sprite2D = $hiddenWorldContainer/LensMask
 @onready var hidden_world_anchor: Node2D = $hiddenWorldContainer/LensMask/HiddenWorldAnchor
 
+@onready var hud = $HUD/CameraUI
+@onready var battery_bar = $HUD/CameraUI/BatteryBar
+@onready var credits_label = $HUD/CameraUI/DataCredits
+@onready var rec_dot = $HUD/CameraUI/RecDot
+
+var data_credits: int = 0
+var max_battery: float = 100.0
+var current_battery: float = 100.0
+var is_aiming: bool = false
+
 func _ready() -> void:
 	lens_mask.visible = false
+	battery_bar.max_value = max_battery
+	battery_bar.value = current_battery
+	update_score_display()
+	hud.visible = false
 
 func _physics_process(_delta: float) -> void:
 	var dir = Input.get_vector("move_left","move_right","move_up","move_down")
@@ -17,7 +31,7 @@ func _physics_process(_delta: float) -> void:
 		velocity = Vector2.ZERO
 	move_and_slide()
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	ray_cast.target_position = get_local_mouse_position()
 	
 	if Input.is_action_just_pressed("aim_camera"):
@@ -28,6 +42,18 @@ func _process(_delta: float) -> void:
 	
 	if Input.is_action_just_pressed("ui_accept"):
 		snap_photo()
+	
+	if Input.is_action_pressed("aim_camera"):
+		is_aiming = true
+		hud.visible = true
+		drain_battery(delta)
+		blink_rec_dot()
+	else:
+		is_aiming = false
+		hud.visible = false
+		
+	if is_aiming and Input.is_action_just_pressed("ui_accept") and current_battery > 10:
+		snap_photo()
 
 func update_lens_position():
 	var target_pos = get_global_mouse_position()
@@ -35,6 +61,7 @@ func update_lens_position():
 	hidden_world_anchor.global_position = Vector2.ZERO
 
 func snap_photo():
+	current_battery -= 10.0
 	ray_cast.force_raycast_update()
 	
 	if ray_cast.is_colliding():
@@ -53,3 +80,18 @@ func save_image_to_disk() -> void:
 	
 	img.save_png(file_path)
 	print("Photo Saved Successfully")
+
+func update_score_display():
+	credits_label.text = "DATA: " + str(data_credits) + " TB"
+	battery_bar.value = current_battery
+
+func blink_rec_dot():
+	var time_msec = Time.get_ticks_msec()
+	if time_msec % 1000 < 500:
+		rec_dot.visible = true
+	else:
+		rec_dot.visible = false
+
+func drain_battery(amount_to_drain):
+	current_battery -= amount_to_drain * 5.0 
+	battery_bar.value = current_battery
